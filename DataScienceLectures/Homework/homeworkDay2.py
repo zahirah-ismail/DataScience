a31a82c9f5bf7da8d4f6cc8d63bbc2eb9ea25fcc
zahirah <zahirah.ismail1@gmail.com>
2018-06-28 12:27:01 +0200

    day4

--- DataScienceLectures/Homework/homeworkDay2.py
+++ DataScienceLectures/Homework/homeworkDay2.py
@@ -1,4 +1,10 @@
-class Ixstudent:
+class Ix:
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
+details = Ix('zahirah', 20)
+
+print(details.name)
---
+++ DataScienceLectures/Homework/homework_day_4.py
@@ -0,0 +1,10 @@
+class Ix:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+details = Ix('zahirah', 20)
+
+print(details.name)
