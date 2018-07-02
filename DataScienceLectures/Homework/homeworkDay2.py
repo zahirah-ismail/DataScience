3d9a0834cebcc7a988fed744e63ee3d825b7a704
zahirah <zahirah.ismail1@gmail.com>
2018-07-02 10:15:07 +0200

    day 5

--- DataScienceLectures/Homework/homeworkDay2.py
+++ DataScienceLectures/Homework/homeworkDay2.py
@@ -4,7 +4,14 @@ class Ix:
         self.name = name
         self.age = age
 
+ def __ilovedsd__(self):
+        print("i love data science")
 
 details = Ix('zahirah', 20)
-
+student1 = Ix('Jeffrey', 22)
+student2 = Ix('Adam', 19)
 print(details.name)
+print(details.age)
+print("my name is: "+details.name+" and i am "+details.age+" old")
+ 
+
